95a7b47816890d0133e2d539f50c426b10281c23
DouglasAraujo0 <douglas.arau@hotmail.com>
2024-04-22 11:56:07 -0300

    finishing AbstractRequest.java

--- src/main/java/br/com/fiap/simuladospringpfunidades/dto/request/AbstractRequest.java
+++ src/main/java/br/com/fiap/simuladospringpfunidades/dto/request/AbstractRequest.java
@@ -1,4 +1,12 @@
 package br.com.fiap.simuladospringpfunidades.dto.request;
 
-public record AbstractRequest() {
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+public record AbstractRequest(
+
+        @Positive(message = "O id deve ser um número positivo")
+        @NotNull(message = "O id deve ser informado")
+        Long id
+) {
 }
