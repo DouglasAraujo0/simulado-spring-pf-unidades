504df599a61903f6f0b6b1f0188135270d1275fb
douglas <douglas.arau@hotmail.com>
2024-04-23 18:29:26 -0300

    package resource finish

--- src/main/java/br/com/fiap/simuladospringpfunidades/resource/ChefeResource.java
+++ src/main/java/br/com/fiap/simuladospringpfunidades/resource/ChefeResource.java
@@ -1,4 +1,33 @@
 package br.com.fiap.simuladospringpfunidades.resource;
 
+import br.com.fiap.simuladospringpfunidades.entity.Chefe;
+import br.com.fiap.simuladospringpfunidades.repository.ChefeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping(value = "/chefe")
 public class ChefeResource {
+
+    @Autowired
+    private ChefeRepository repo;
+
+    @GetMapping
+    public Collection<Chefe> findAll(){
+        return repo.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public Chefe findById(@PathVariable Long id) {
+        return repo.findById(id).orElseThrow();
+    }
+
+    @Transactional
+    @PostMapping
+    public Chefe save(@RequestBody Chefe chefe) {
+        return repo.save(chefe);
+    }
 }
--- src/main/java/br/com/fiap/simuladospringpfunidades/resource/UsuarioResource.java
+++ src/main/java/br/com/fiap/simuladospringpfunidades/resource/UsuarioResource.java
@@ -1,4 +1,34 @@
 package br.com.fiap.simuladospringpfunidades.resource;
 
+import br.com.fiap.simuladospringpfunidades.entity.Usuario;
+import br.com.fiap.simuladospringpfunidades.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping(value = "/usuario")
 public class UsuarioResource {
+
+    @Autowired
+    private UsuarioRepository repo;
+
+    @GetMapping
+    public Collection<Usuario> findAll(){
+        return repo.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public Usuario findById(@PathVariable Long id) {
+        return repo.findById(id).orElseThrow();
+    }
+
+    @Transactional
+    @PostMapping
+    public Usuario save(@RequestBody Usuario usuario) {
+        return repo.save(usuario);
+    }
 }
+
